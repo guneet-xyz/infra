fb733657fc2ab51503f80e7a3f6e0e4af5666f4a
Guneet Aggarwal <guneetaggarwal@gmail.com>
2026-05-01 09:21:52 +0530

    chore: remove legacy Docker Compose services

--- services/db/upfresh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-
-cd /mnt/infra/services/db
-#infisical export --output-file '.env' --env prod
-docker compose up -d
--- services/easyshell/refresh.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-cd /mnt/infra/services/easyshell
-docker compose up -d
